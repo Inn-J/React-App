3b9f8f47f37ad923fc99c1c4c17fdb188a2127e6
Inn-J <inn.javanicha@gmail.com>
2025-10-12 15:53:26 +0700

    Lab9+10

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 import Navbar from './features/Navbar';
 import Container from './features/Container';
 import Home from './features/Home';
+import GlobalStyle from './features/GlobalStyle';
 
 function App() {
   return (
     <>
+    <GlobalStyle />
       <Navbar />
       <Container>
         <Home />
       </Container>
     </>
   );
- //return <h1>Hello React</h1>
 }
 
 export default App;
--- src/features/Home.js
+++ src/features/Home.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 //import data from '../app/data';
 import Product from './Product';
 import AddForm from './Product/AddForm';
 import axios from 'axios';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 let currentProductId = 9;
-function Home() {
+function Home({ className }) {
   const [products, setProducts] = useState([]);
+  useEffect(() => {
   async function getProducts() {
-  const products = await axios.get(
-    'https://68e9f9e1f1eeb3f856e59831.mockapi.io/api/products'
-  );
-  setProducts(products.data);
-}
-getProducts();
+    const products = await axios.get(
+      'https://68e9f9e1f1eeb3f856e59831.mockapi.io/api/products'
+    );
+    setProducts(products.data);
+  }
+
+  getProducts();
+}, []);
+
   function addProduct(product) {
   const newProduct = { id: ++currentProductId, ...product };
   setProducts([...products, newProduct]);
 }
   return (
-  <>
-  <h1>New Products</h1>
+  <div className={className}>
+  <h1 >New Products</h1>
   {
   products.length > 0 ? (
     <ul className="Home__products">
@@ -33,10 +39,20 @@ getProducts();
   )
 }
   <AddForm addProduct={addProduct} />
-</>);
+</div>);
 }
+Home.propTypes = {
+  className: PropTypes.string.isRequired,
+};
+export default styled(Home)`
+  .Home__products {
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+    margin: 0 -12px;
+  }
+`;
 
 
 
-
-export default Home;
